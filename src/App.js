f1a30c70f18ced620fae1c64b0639c8bd9ff1262
Ryan Canfield <ryancanfield@me.com>
2021-01-28 17:50:08 -0800

    Add new transition switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Header from './Components/Header';
 import Routes from './Routes';
-import ScrollToTop from './Services/ScrollToTop';
 import './App.scss';
 
 const App = (props) => {
   return (
     <div className='app'>
-      <ScrollToTop />
       <div className='l'>
         <div className='l--rightColumn'>
           <a className='visually-hidden' href='#content'>
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
-import { useRef } from 'react';
-import { Route, Switch } from 'react-router';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Route } from 'react-router';
+import { TransitionSwitch } from 'transition-switch';
+
 // PAGES
 import About from './Routes/About';
 import Writing from './Routes/Writing';
@@ -8,26 +8,15 @@ import Contact from './Routes/Contact';
 import Work from './Routes/Work';
 import NotFound from './Routes/NotFound';
 
-const Routes = (props) => {
-  const ref = useRef(null);
-
+const Routes = () => {
   return (
-    <TransitionGroup id='TransitionGroup'>
-      <CSSTransition
-        nodeRef={ref}
-        key={props.location.key}
-        classNames='fade'
-        timeout={{ enter: 0, exit: 0 }}
-      >
-        <Switch location={props.location}>
-          <Route exact path='/' component={About} />
-          <Route exact path='/writing' component={Writing} />
-          <Route exact path='/work' component={Work} />
-          <Route exact path='/contact' component={Contact} />
-          <Route component={NotFound} />
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <TransitionSwitch transition='fade-down'>
+      <Route exact path='/' component={About} />
+      <Route exact path='/writing' component={Writing} />
+      <Route exact path='/work' component={Work} />
+      <Route exact path='/contact' component={Contact} />
+      <Route component={NotFound} />
+    </TransitionSwitch>
   );
 };
 
